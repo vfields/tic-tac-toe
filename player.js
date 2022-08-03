3cbf3064cfaf73052a70ac5fb88ba59acb9609d5
Victoria Fields <victoriaashleyfields@gmail.com>
2022-08-02 20:48:51 -0600

    Experiment with keeping track of turns

--- player.js
+++ player.js
@@ -2,10 +2,23 @@ class Player {
   constructor(id, token) {
     this.id = id;
     this.token = token;
+    this.turn = false;
     this.wins = 0;
   }
   increaseWins() {
     this.wins += 1;
     // this.wins++
+  }
+  /* brainstorm */
+  switchTurn() {
+    if (!this.turn) {
+      this.turn = true;
+    }
+    else if (this.turn) {
+      this.turn = false;
+    }
+  }
+  pickSquare(square) {
+    
   }
 }
