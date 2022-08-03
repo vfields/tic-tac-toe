d83c5b5f4b257a91e5e8e07f34df26e9057dd49e
Victoria Fields <victoriaashleyfields@gmail.com>
2022-08-02 20:23:13 -0600

    Add way to track game board data experiment

--- game.js
+++ game.js
@@ -1,3 +1,17 @@
 class Game {
-  
+  constructor() {
+    this.playerOne = new Player('playerOne', '🐶');
+    this.playerTwo = new Player('playerTwo', '🐱');
+    this.squareOne = 'empty';
+    this.squareTwo = 'empty';
+    this.squareThree = 'empty';
+    this.squareFour = 'empty';
+    this.squareFive = 'empty';
+    this.squareSix = 'empty';
+    this.squareSeven = 'empty';
+    this.squareEight = 'empty';
+    this.squareNine = 'empty';
+    // could this be an array of squares?
+    // this.squares = [];
+  }
 }
