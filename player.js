0952475ee018285abd41ce5f9ee2f1c9d7c3ee3d
Victoria Fields <victoriaashleyfields@gmail.com>
2022-08-04 16:57:59 -0600

    Delete code comments

--- player.js
+++ player.js
@@ -2,11 +2,9 @@ class Player {
   constructor(id, token) {
     this.id = id;
     this.token = token;
-    // this.turn = false;
     this.wins = 0;
   }
   increaseWins() {
     this.wins += 1;
-    // this.wins++
   }
 }
